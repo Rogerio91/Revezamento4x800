2659dfa88bae9a5eb09d602556bf6cbe669105bf
*Rogerio91* <*rogerio.santana@faciplac.edu.br*>
2018-03-14 17:46:45 -0300

    Configurando Travis BrasiiiiiL

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe UsersController, type: :controller do
       user = User.create(name: "Orcestra", email: "projetos@orcestra.com.br")
       get :index, params: {}, session: valid_session
       expect(response).to be_success
-      #expect(nil).to be_success
     end
   end
 
@@ -23,7 +22,6 @@ RSpec.describe UsersController, type: :controller do
          expect {
            post :create, params: {user: valid_attributes}
          }.to change(User, :count).by(2)
-        #expect(nil).to be_success
       end
 
       it "renders a JSON response with the new user" do
@@ -32,7 +30,6 @@ RSpec.describe UsersController, type: :controller do
          expect(response).to have_http_status(:created)
          expect(response.content_type).to eq('application/json')
          expect(response.location).to eq(user_url(User.last))
-        #expect(nil).to be_success
       end
     end
   end
